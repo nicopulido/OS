34b5f63fe8eb818f17e8808841f7e60fa547129d
Nicolás Pulido <nicopipe2010@gmail.com>
2026-03-11 21:05:28 -0500

    feat: add executeSimulation method to manage CPU process execution

--- Process-state-simulator/CPU.js
+++ Process-state-simulator/CPU.js
@@ -7,4 +7,14 @@ class CPU {
         this.currentPCB = pcb;
     }
 
+    executeSimulation() {
+        if (this.currentPCB) {
+            console.log(`Executing process ${this.currentPCB.process.name} with PID ${this.currentPCB.pid}.`);
+            this.currentPCB.programCounter += 1;
+            console.log(`Process ${this.currentPCB.process.name} with PID ${this.currentPCB.pid} has program counter at ${this.currentPCB.programCounter}.`);
+        } else {
+            console.log("No process is currently running on the CPU.");
+        }
+    }
+
 }
