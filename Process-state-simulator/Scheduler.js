0e9c79250b1e6017166281728c9a08aa1f5fb503
Nicolás Pulido <nicopipe2010@gmail.com>
2026-03-11 20:40:53 -0500

    feat: implement Scheduler class with process selection method

---
+++ Process-state-simulator/Scheduler.js
@@ -0,0 +1,15 @@
+class Scheduler {
+
+    
+    selectNextProcess(processes) {
+        const readyProcesses = processes.filter(pcb => pcb.state === PROCESS_STATES.READY);
+        if (readyProcesses.length === 0) {
+            console.log("No processes in READY state to schedule.");
+            return null;
+        }
+        // We use first-come, first-served (FCFS) scheduling because we have just seen this algorithm in class
+        const nextProcess = readyProcesses.shift();
+        console.log(`Scheduler selected process ${nextProcess.process.name} with PID ${nextProcess.pid} for execution.`);
+        return nextProcess;
+    }
+}
