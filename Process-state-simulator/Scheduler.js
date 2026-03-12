7a9d0a6f5b8291b98fcd637e59bc4d890570a6b8
Nicolás Pulido <nicopipe2010@gmail.com>
2026-03-11 23:59:43 -0500

    feat: optimize process selection logic in Scheduler class

--- Process-state-simulator/Scheduler.js
+++ Process-state-simulator/Scheduler.js
@@ -4,16 +4,14 @@ export default class Scheduler {
 
     
     selectNextProcess(processes) {
-        const readyIndex = processes.findIndex(
+        const nextProcess = processes.find(
             (pcb) => pcb.state === PROCESS_STATES.READY
         );
 
-        if (readyIndex === -1) {
+        if (!nextProcess) {
             console.log('No processes in READY state to schedule.');
             return null;
         }
-        
-        const [nextProcess] = processes.splice(readyIndex, 1);
 
         console.log(
             `Scheduler selected process ${nextProcess.process.name} with PID ${nextProcess.pid} for execution.`
