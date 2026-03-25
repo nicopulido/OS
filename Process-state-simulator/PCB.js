eaad3b1c5124012ffaad6188df15d8d97e9d80b5
Nicolás Pulido <nicopipe2010@gmail.com>
2026-03-25 16:39:55 -0500

    feat: add remainingExecutionTime property and update methods

--- Process-state-simulator/PCB.js
+++ Process-state-simulator/PCB.js
@@ -6,5 +6,15 @@ export default class PCB {
         this.pid = process.id;
         this.state = PROCESS_STATES.NEW;
         this.programCounter = 0;
+        this.remainingExecutionTime = process.executionTime;
+    }
+    
+    updateState(newState) {
+        this.state = newState;
+    }
+
+    updateRemainingExecutionTime(time) {
+        this.remainingExecutionTime = Math.max(0, this.remainingExecutionTime - time);
+        // Ensure remaining execution time does not go below zero
     }
 }
