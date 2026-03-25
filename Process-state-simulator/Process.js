47eafdae8e14e9dbc60c316dd90f5fa782a9e0de
Nicolás Pulido <nicopipe2010@gmail.com>
2026-03-25 17:01:56 -0500

    refactor: improve addBlockingEvent method

--- Process-state-simulator/Process.js
+++ Process-state-simulator/Process.js
@@ -1,8 +1,19 @@
 export default class Process {
-    constructor(id, name, executionTime, initiationTime, ) {
+    constructor(id, name, executionTime, initiationTime) {
         this.id = id; // Unique identifier for the process
         this.name = name; // Name of the process 
         this.executionTime = executionTime; // Total time required for the process to complete execution
         this.initiationTime = initiationTime; // Time at which the process is initiated
+        this.blockingEvents = []; // List of blocking events for the process
+    }
+
+    addBlockingEvent(startTime, duration) {
+        // Ensure that the blocking event's start time is within the execution time of the process
+        if(startTime < this.executionTime && startTime >= 0) {
+            const event = new BlockingEvent(startTime, duration);
+            this.blockingEvents.push(event);
+        } else {
+            console.error(`Invalid blocking event start time: ${startTime}. It must be between 0 and the process's execution time.`);
+        }
     }
 }
