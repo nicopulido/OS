6826b890e31b0897455ea47c772a72bc4781a574
Nicolás Pulido <nicopipe2010@gmail.com>
2026-03-25 16:33:26 -0500

    refactor: remove selectNextProcess method from Scheduler class

--- Process-state-simulator/Scheduler.js
+++ Process-state-simulator/Scheduler.js
@@ -3,19 +3,4 @@ import PROCESS_STATES from './PROCESS_STATES.js';
 export default class Scheduler {
 
     
-    selectNextProcess(processes) {
-        const nextProcess = processes.find(
-            (pcb) => pcb.state === PROCESS_STATES.READY
-        );
-
-        if (!nextProcess) {
-            console.log('No processes in READY state to schedule.');
-            return null;
-        }
-
-        console.log(
-            `Scheduler selected process ${nextProcess.process.name} with PID ${nextProcess.pid} for execution.`
-        );
-        return nextProcess;
-    }
 }
