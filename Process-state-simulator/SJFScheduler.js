ed9534dae68a23c97745bb21a6f956e6ebb451ff
Nicolás Pulido <nicopipe2010@gmail.com>
2026-03-25 17:56:23 -0500

    feat: implement SJFScheduler class with getNextProcess method for shortest job first scheduling

---
+++ Process-state-simulator/SJFScheduler.js
@@ -0,0 +1,21 @@
+export default class SJFScheduler extends Scheduler {
+    constructor() {
+        super();
+    }
+
+    getNextProcess(readyProcesses) {
+        // Shortest Job First scheduling algorithm
+        let selectedPCB = readyProcesses[0] || null;
+        for (let pcb of readyProcesses) {
+            if (pcb.process.executionTime < selectedPCB.process.executionTime) {
+                selectedPCB = pcb; // Update selected PCB if it has a shorter execution time
+            }else if (pcb.process.executionTime === selectedPCB.process.executionTime) {
+                // If execution times are equal, use arrival time as a tiebreaker
+                if (this.selectByArrivalTime(pcb, selectedPCB) < 0) {
+                    selectedPCB = pcb;
+                }
+            }
+        }
+        return selectedPCB;
+    }
+}
