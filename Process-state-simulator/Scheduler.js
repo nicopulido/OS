f8795256bfb183ae2008c4a161fa910baaf93fad
Nicolás Pulido <nicopipe2010@gmail.com>
2026-03-25 17:14:32 -0500

    feat: implement getNextProcess method to return the first process in the ready queue

--- Process-state-simulator/Scheduler.js
+++ Process-state-simulator/Scheduler.js
@@ -2,5 +2,8 @@ import PROCESS_STATES from './PROCESS_STATES.js';
 
 export default class Scheduler {
 
-    
+    getNextProcess(readyProcesses) {
+        // Default implementation - returns the first process in the ready queue
+        return readyProcesses.length > 0 ? readyProcesses[0] : null;
+    }
 }
