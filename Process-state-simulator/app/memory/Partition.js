604a4b279ec3217e82b07ceac5b979b35b1a6474
Nicolás Pulido <nicopipe2010@gmail.com>
2026-04-25 19:26:24 -0500

    refactor: rename MemoryManager to Partition and adjust constructor

--- Process-state-simulator/app/memory/Partition.js
+++ Process-state-simulator/app/memory/Partition.js
@@ -1,7 +1,11 @@
-export class MemoryManager {
-    constructor(PID, base, limit) {
-        this.PID = PID;
+export class Partition {
+    constructor(base, limit) {
+        this.PID = 0;
         this.base = base;
         this.limit = limit;
+    }
+
+    setPID(PID) {
+        this.PID = PID;
     }
 }
